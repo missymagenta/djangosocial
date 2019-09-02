bab918fa8a0de9bfe3ee5b5302d822b716e9d744
missymagenta <alina.soare37@gmail.com>
2019-09-02 18:44:27 +0200

    last commit sept3

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# edit models on the same page as a parent model
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
